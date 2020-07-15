c35f942d958fe114541eb59fbc89f57a4ac712cc
Hung Nguyen <hungnt89@gmail.com>
2020-07-16 01:31:04 +0700

    Xem duoc xy cua elems.

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const tasks = document.querySelectorAll(".task");
 
 tasks.forEach((task) => {
+  const box = task.getBoundingClientRect();
   task.addEventListener("dragstart", (e) => {
     task.classList.add("dragging");
   });
   task.addEventListener("dragend", () => {
     task.classList.remove("dragging");
+  });
+  task.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    if (e.offsetY <= box.height / 2) {
+      console.log("Tren " + task.textContent);
+    } else if (e.offsetY > box.height / 2) {
+      console.log("Duoi " + task.textContent);
+    }
   });
 });
