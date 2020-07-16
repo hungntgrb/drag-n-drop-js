229157d118ae9b67d6971059c4777f086e9ac84f
Hung Nguyen <hungnt89@gmail.com>
2020-07-16 13:36:30 +0700

    Can drag drop from Box1 to Box2. Cannot drop into empty Box.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const tasks = document.querySelectorAll(".task");
+const containers = document.querySelectorAll(".container");
+
+underneathElem = null;
 
 tasks.forEach((task) => {
   const box = task.getBoundingClientRect();
@@ -7,13 +10,28 @@ tasks.forEach((task) => {
   });
   task.addEventListener("dragend", () => {
     task.classList.remove("dragging");
+    insertItBefore(task, underneathElem);
+    underneathElem = null;
   });
   task.addEventListener("dragover", (e) => {
     e.preventDefault();
-    if (e.offsetY <= box.height / 2) {
-      console.log("Tren " + task.textContent);
-    } else if (e.offsetY > box.height / 2) {
-      console.log("Duoi " + task.textContent);
+    if (isBefore(box, e)) {
+      underneathElem = task;
     }
   });
 });
+
+const t1 = tasks[0];
+const c1 = containers[0];
+const c2 = containers[1];
+const t3 = tasks[2];
+const t4 = tasks[3];
+
+function isBefore(box, event) {
+  return event.offsetY <= box.height / 2;
+}
+
+function insertItBefore(newE, existing) {
+  const parent = existing.parentElement;
+  parent.insertBefore(newE, existing);
+}
